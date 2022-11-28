130589b199378906ccfc2a8ef7757da3dcc61243
maxwen <max.weninger@gmail.com>
2022-11-28 20:58:19 +0100

    use accent1_100 instead of 200 for dynamic primary dark

--- app/src/main/java/org/omnirom/omnigerrit/ui/theme/Theme.kt
+++ app/src/main/java/org/omnirom/omnigerrit/ui/theme/Theme.kt
@@ -4,13 +4,10 @@ import android.app.Activity
 import android.content.res.Configuration
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -57,7 +54,9 @@ fun OmniGerritTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context).copy(
+                primary = Color(context.resources.getColor(android.R.color.system_accent1_100, context.theme))
+            ) else dynamicLightColorScheme(context)
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
